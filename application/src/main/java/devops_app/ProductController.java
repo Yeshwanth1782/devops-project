eaf75463b6ca3a76c9a10bb012ab44e7c6ea4861
Yeshwanth1782 <yeshwanth1782@gmail.com>
2026-09-16 11:29:11 +0530

    Add MySQL with Docker Compose

---
+++ application/src/main/java/devops_app/ProductController.java
@@ -0,0 +1,21 @@
+package devops_app;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ProductController {
+
+    private final ProductRepository productRepository;
+
+    public ProductController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @GetMapping("/api/products")
+    public List<Product> getProducts() {
+        return productRepository.findAll();
+    }
+}
---
+++ application/src/main/java/devops_app/ProductRepository.java
@@ -0,0 +1,6 @@
+package devops_app;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
